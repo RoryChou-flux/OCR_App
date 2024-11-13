22ebae40a986fd642415f346d6f060eefb6c1f40
Rorychouchin <11864342+rorychouchin@user.noreply.gitee.com>
2024-11-13 10:14:44 +0800

    latex和document空文件，待完善

---
+++ app/src/main/java/com/example/vision/DocumentActivity.java
@@ -0,0 +1,12 @@
+package com.example.vision;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DocumentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: 实现文档处理逻辑
+    }
+}
---
+++ app/src/main/java/com/example/vision/LatexActivity.java
@@ -0,0 +1,12 @@
+package com.example.vision;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LatexActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: 实现 LaTeX 处理逻辑
+    }
+}
